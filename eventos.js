6765db9a06185ad6d907252bcbb38b08f7a56868
gabrielricardosilva <gabrielricardo.silva17@gmail.com>
2022-04-25 09:18:21 -0300

    first commit

---
+++ eventos.js
@@ -0,0 +1,15 @@
+function clickMe(){
+    console.log("Que demais")
+}
+function FocusMe(){
+    console.log("Foca em Mim")
+}
+function blurMe(){
+    console.log ("Fui Blurado")
+}
+function changeMe(){
+    console.log ("Estou muy Alterado")
+}
+function overMe(){
+    console.log("Mouse aqui")
+}
